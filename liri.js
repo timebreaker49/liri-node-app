d8fbd52176fb8da4c1c0fd1b01c82b23af5bdca1
Wayne Baylor <wcsbaylor@gmail.com>
2018-04-02 23:39:09 -0500

    refactored to take in command line arguments and implemented OMDB search

--- liri.js
+++ liri.js
@@ -4,15 +4,30 @@ require("dotenv").config();
 
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
-var Request = require("request");
+var Request = require("request-promise");
+var omdbApi = require("omdb-api-pt");
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
+var omdb = new omdbApi({
+    apiKey: process.env.OMDB_KEY,
+    baseUrl : "http://www.omdbapi.com/" //+ input + "&r=json&plot=short&apikey=" + apikey,
+})
 
 var sn = {screen_name: 'whutturfacts'};
+var userSearchArray = [];
 
 var arg = process.argv[2];
+var userSearch = function() {
+        for (let i = 3; i < process.argv.length; i++) {
+            userSearchArray.push(process.argv[i]);
+        } 
+        var title = userSearchArray.join(' ');
+        return title;
+}
+var searchTerm = userSearch();
+console.log(searchTerm);
 
 switch(arg) {
     case('my-tweets'):
@@ -24,6 +39,7 @@ switch(arg) {
 //I don't have 20 tweets yet, so undefined is returned after data retrievable is exhausted
     });
         break;
+
     case('spotify-this-song'):
     spotify.search({ type: 'track', query: 'alright', limit: 1 }, function(err, data) {
           if (err) {
@@ -35,24 +51,20 @@ switch(arg) {
           console.log("Name of the song: " + JSON.stringify(data.tracks.items[0].name, null, 2));
           console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
           console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
-          }
+       
+        }
+    });
         break;
+
     case('movie-this'):
+        // promise request for OMDB search 
+            var searchOMDB = omdb.byId({
+                title: searchTerm
+            }).then(res => console.log(res))
+            .catch(err => console.error(err));
 
         break;
     case('do-what-it-says'):
 
         break;
 }
-
-
-
-//spotify.search({ type: 'track', query: 'frozen', limit: 1 }, function(err, data) {
-//  if (err) {
-//    return console.log('Error occurred: ' + err);
-//  }
-//
-//console.log(JSON.stringify(data.tracks.items.uri, null, 2));
-//console.log(JSON.stringify(data.tracks.items.album, null, 2));
-//})
-
