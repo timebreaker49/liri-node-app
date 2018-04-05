1e8f55f8d9cb8ce945264f4fad50297e28520c96
Wayne Baylor <wcsbaylor@gmail.com>
2018-04-04 23:02:01 -0500

    prettified, now need to refactor and clean up, final checks

--- liri.js
+++ liri.js
@@ -13,102 +13,113 @@ let spotify = new Spotify(keys.spotify);
 let twitter = new Twitter(keys.twitter);
 let omdb = new omdbApi({
     apiKey: process.env.OMDB_KEY,
-    baseUrl : "http://www.omdbapi.com/" //+ input + "&r=json&plot=short&apikey=" + apikey,
+    baseUrl: "http://www.omdbapi.com/" //+ input + "&r=json&plot=short&apikey=" + apikey,
 })
 
-let sn = {screen_name: 'whutturfacts'};
+let sn = {
+    screen_name: 'whutturfacts'
+};
 let userSearchArray = [];
-//from file to for do-what-it-says functionality, not yet working
-// let fromFile = false;
-// console.log(fromFile);
 
 let arg = process.argv[2];
-let userSearch = function() {
-        for (let i = 3; i < process.argv.length; i++) {
-            userSearchArray.push(process.argv[i]);
-        } 
-        let title = userSearchArray.join(' ');
-        return title;
+let userSearch = function () {
+    for (let i = 3; i < process.argv.length; i++) {
+        userSearchArray.push(process.argv[i]);
+    }
+    let title = userSearchArray.join(' ');
+    return title;
 }
 
 let searchTerm = userSearch();
 
 let command = function () {
-    switch(arg) {
-        case('my-tweets'):
-        twitter.get("statuses/user_timeline", sn, function(error, tweets, response) {
-        for (let i = 0; i < 20; i++) {
-        console.log("When he dropped knowledge: " + JSON.stringify(tweets[i].created_at, null, 2)); //tweet "created_at" property
-        console.log("The wisdom imparted upon us: " + JSON.stringify(tweets[i].text, null, 2)); //tweet "text" property
-        }
-    //I don't have 20 tweets yet, so undefined is returned after data retrievable is exhausted
-        });
+    switch (arg) {
+        case ('my-tweets'):
+            twitter.get("statuses/user_timeline", sn, function (error, tweets, response) {
+                for (let i = 0; i < 20; i++) {
+                    console.log("When he dropped knowledge: " + JSON.stringify(tweets[i].created_at, null, 2)); //tweet "created_at" property
+                    console.log("The wisdom imparted upon us: " + JSON.stringify(tweets[i].text, null, 2)); //tweet "text" property
+                }
+                //I don't have 20 tweets yet, so undefined is returned after data retrievable is exhausted
+            });
+
             break;
 
-        case('spotify-this-song'):
-            
-        let spotifyLookup = function () {
-            spotify.search({ type: 'track', query: searchTerm, limit: 1 }, function(err, data) {
-                if (err) {
-                    return console.log('Error occurred: ' + err);
-                }
-        //maybe consider using backticks to format the console.log statement
-                for (let i = 0; i < 1; i++) {
-                console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
-                console.log("Name of the song: " + JSON.stringify(data.tracks.items[0].name, null, 2));
-                console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
-                console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
-            
-                }
-            });    
-        } ();
+        case ('spotify-this-song'):
+
+            let spotifyLookup = function () {
+                spotify.search({
+                    type: 'track',
+                    query: searchTerm,
+                    limit: 1
+                }, function (err, data) {
+                    if (err) {
+                        return console.log('Error occurred: ' + err);
+                    }
+                    //maybe consider using backticks to format the console.log statement
+                    for (let i = 0; i < 1; i++) {
+                        console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+                        console.log("Name of the song: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+                        console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
+                        console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
+
+                    }
+                });
+            }();
+
             break;
 
-        case('movie-this'):
+        case ('movie-this'):
             // promise request for OMDB search 
 
             if (!searchTerm) {
                 searchTerm = "Mr. Nobody"
-            } 
+            }
             let searchOMDB = omdb.byId({
-                title: searchTerm
-            }).then(res => console.log(res))
-            .catch(err => console.error(err));
+                    title: searchTerm
+                }).then(res => console.log(res))
+                .catch(err => console.error(err));
 
             break;
 
-        case('do-what-it-says'):
-            fs.readFile('random.txt', 'utf8', function(err, data) {
+        case ('do-what-it-says'):
+            fs.readFile('random.txt', 'utf8', function (err, data) {
                 if (err) {
                     return console.log(err);
                 }
                 let splitter = data.split(',');
                 let arg = splitter[0];
                 let fileSearchTerm = splitter[1];
-                
+
+                //command();                              
+                //if the above does not work -- the nuclear option available below :(
                 if (arg === "spotify-this-song") {
-                    spotify.search({ type: 'track', query: fileSearchTerm, limit: 1 }, function(err, data) {
+                    spotify.search({
+                        type: 'track',
+                        query: fileSearchTerm,
+                        limit: 1
+                    }, function (err, data) {
                         if (err) {
                             return console.log('Error occurred: ' + err);
                         }
-                //maybe consider using backticks to format the console.log statement
+                        //maybe consider using backticks to format the console.log statement
                         for (let i = 0; i < 1; i++) {
-                        console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
-                        console.log("Name of the song: " + JSON.stringify(data.tracks.items[0].name, null, 2));
-                        console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
-                        console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
-                    
+                            console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+                            console.log("Name of the song: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+                            console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
+                            console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
+
                         }
                     });
                 } else {
                     let searchOMDB = omdb.byId({
-                        title: fileSearchTerm
-                    }).then(res => console.log(res))
-                    .catch(err => console.error(err));
+                            title: searchTerm
+                        }).then(res => console.log(res))
+                        .catch(err => console.error(err));
                 }
             })
-    
+
             break;
     }
 }
-command()
+command();
